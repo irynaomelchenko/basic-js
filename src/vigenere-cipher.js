3344663cf5f9cf023f7a9ba17447bcafe2627063
Iryna Omelchenko <iren.omelchenko@gmail.com>
2021-03-25 15:41:39 +0200

    feat: implement solution for Vigenere cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,83 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(boolean) {
+    this.typeOfMachine = boolean
   }
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, k) {
+
+    if (!message || !k) throw new Error('Missing parameter/s');
+
+    let a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let res = '';
+    let m = message.split(' ').join('').toUpperCase()
+    // const maxLength = Math.max(m.length, k.length);
+    let n = [];
+    let arrMes = message.split('');
+
+    for (let i = 0; i < arrMes.length; i++) {
+      if (arrMes[i] === ' ') {
+        n.push(i)
+      }
+    };
+
+    for (let i = 0; i < m.length; i++) {
+      // let mi = a.indexOf(m[i >= m.length ? i % m.length : i])
+      let mi = a.indexOf(m[i])
+      let ki_s = k.toUpperCase()[i >= k.length ? i % k.length : i]
+      let ki = a.indexOf(ki_s)
+      let c = mi === (-1) ? m[i] : a[(a.length + mi + ki) % a.length]
+      res += c
+    };
+
+    for(let i=0;i<n.length;i++){
+      let resArr=res.split('')
+      resArr.splice(n[i],0,' ')
+      res=resArr.join('')
+    }
+
+    if (this.typeOfMachine === undefined || this.typeOfMachine) {
+      return res;
+    } else if (this.typeOfMachine === false) {
+      return res.split('').reverse().join('')
+    }
+  }
+  decrypt(encMessage, k) {
+    if (!encMessage || !k) throw new Error('Missing parameter/s');
+    let a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let res = '';
+    let m = encMessage.split(' ').join('').toUpperCase()
+    // const maxLength = Math.max(m.length, k.length);
+    let n = [];
+    let arrMes = encMessage.split('');
+
+    for (let i = 0; i < arrMes.length; i++) {
+      if (arrMes[i] === ' ') {
+        n.push(i)
+      }
+    };
+
+    for (let i = 0; i < m.length; i++) {
+      let mi = a.indexOf(m[i])
+      let ki_s = k.toUpperCase()[i >= k.length ? i % k.length : i]
+      let ki = -(a.indexOf(ki_s))
+      let c = mi === (-1) ? m[i] : a[(a.length + mi + ki) % a.length]
+      res += c
+    };
+
+    for(let i=0;i<n.length;i++){
+      let resArr=res.split('')
+      resArr.splice(n[i],0,' ')
+      res=resArr.join('')
+    }
+
+    if (this.typeOfMachine === undefined || this.typeOfMachine) {
+      return res.toUpperCase();
+    } else if (this.typeOfMachine === false) {
+      return res.split('').reverse().join('').toUpperCase()
+    }
   }
 }
 
